55ee5ff942b0b2d0a841e8517bdda210cf7d9b15
Timo Spring <timo.spring@students.unibe.ch>
2019-03-25 17:12:38 +0100

    Loading MNIST dataset, Closes #2

--- SVM.py
+++ SVM.py
@@ -1,1 +1,27 @@
-# Placeholder file for SVM Task
+# Use Python 3.7
+
+import numpy as np
+
+
+def main():
+    train = FileLoader('train.csv')
+    data = train.data
+    test = train.test
+
+
+class FileLoader:
+    def __init__(self, filename):
+        csvfile = self.load(filename)
+        cols = len(csvfile[0])
+        self.target = csvfile[:, 0]
+        self.data = csvfile[:, 1:cols]
+
+    @staticmethod
+    def load(filename):
+        print('Loading data...')
+        csvfile = np.loadtxt(filename, delimiter=',', dtype=int)
+        return csvfile
+
+
+if __name__ == '__main__':
+    main()
