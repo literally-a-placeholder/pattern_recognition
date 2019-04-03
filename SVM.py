971f5d06f10e5c960168161b2548b557819d06ed
Timo Spring <timo.spring@students.unibe.ch>
2019-04-03 18:19:20 +0200

    Modified SVM

--- SVM.py
+++ SVM.py
@@ -6,32 +6,17 @@ from sklearn.model_selection import GridSearchCV
 
 
 def main():
-    train = FileLoader('./datasets/mnist_train_dev.csv')  # TODO replace with real files
-    test = FileLoader('./datasets/mnist_test_dev.csv') # TODO replace with real files
-    n_folds = 10
-    default_perf = run_std_svm(train, test)
+    train = FileLoader('./datasets/mnist_train.csv')  # TODO replace with real files
+    test = FileLoader('./datasets/mnist_test.csv') # TODO replace with real files
+    n_folds = 5
     classifier = parameter_tuning(train, n_folds=n_folds)
     validation_score = round(validate(classifier, test), 3)*100
-    print_results(default_perf, classifier, validation_score)
-
-
-def run_std_svm(train, test):
-    print('Running standard SVM before parameter tuning...')
-    linear_SVM = svm.SVC(kernel='linear')
-    rbf_SVM = svm.SVC()  # default is rbf
-
-    linear_SVM.fit(train.data, train.target)
-    linear_test_score = round(linear_SVM.score(test.data, test.target), 3)*100
-
-    rbf_SVM.fit(train.data, train.target)
-    rbf_test_score = round(rbf_SVM.score(test.data, test.target), 3)*100
-
-    return (linear_test_score, rbf_test_score)
+    print_results(classifier, validation_score)
 
 
 def parameter_tuning(train, n_folds):
     print('Started parameter tuning...')
-    Cs = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
+    Cs = [0.1, 1, 10, 100, 1000]
     gammas = [0.001, 0.01, 0.1, 1]
     params = [
         {'kernel': ['linear'], 'C': Cs},
@@ -49,11 +34,7 @@ def validate(classifier, test):
     return classifier.score(test.data, test.target)
 
 
-def print_results(default_perf, classifier, validation_score):
-    print('Accuracy before parameter tuning for: ')
-    print('\tlinear kernel: ', default_perf[0], '%')
-    print('\trbf kernel: ', default_perf[1], '%\n')
-
+def print_results(classifier, validation_score):
     print('Cross Validation Results:')
     print('\tAverage accuracy during cross-validation: ', round(classifier.best_score_, 3)*100, '%')
     print('\tBest Kernel:', classifier.best_estimator_.kernel)
@@ -62,16 +43,6 @@ def print_results(default_perf, classifier, validation_score):
         print('\tBest Gamma:', classifier.best_estimator_.gamma)
 
     print('\nTotal accuracy on test set: ', validation_score, '%')
-    print('\tAccuracy increase using parameter tuning:', validation_score - max(default_perf), '%')
-
-
-def whatever(accuracy, classifier):
-    print(' Accuracy on Test Set: ', round(accuracy, 4) * 100, '% for:')
-    print('Best Kernel:', classifier.best_estimator_.kernel)
-    print('Best C:', classifier.best_estimator_.C)
-    if classifier.best_estimator_.kernel == 'rbf':
-        print('Best Gamma:', classifier.best_estimator_.gamma)
-    print(classifier.cv_results_)
 
 
 class FileLoader:
